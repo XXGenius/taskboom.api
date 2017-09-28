88042e12d223953c84c864184783aab9b1a33e5f
djedai.genius@gmail.com <djedai.genius@gmail.com>
2017-09-28 19:42:56 +0300

    Кастомная авторизация и регистрация

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,12 +15,14 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     public function register(Request $request){
-        $user = User::create($request->all());
         $password = $request->input('password');
         if(!$password){
             return response()->json('Enter the password!');
+        }else{
+            $user = User::create($request->all());
+            return response()->json($user);
         }
-        return response()->json($user);
+
     }
 
     public function login(Request $request)
