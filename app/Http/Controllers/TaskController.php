ecc45ba0b5a5a87c00c87c183bab89d742d9599c
djedai.genius@gmail.com <djedai.genius@gmail.com>
2017-10-10 16:58:31 +0300

    findTaskByDate

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -16,7 +16,7 @@ class TaskController extends Controller
     {
         $token = $request->input('token');
         if($token == $this->token){
-            $tasks = Task::where('created_at','like',$date + '%'  )->get();
+            $tasks = Task::where('created_at','like','%' +$date + '%'  )->get();
             return response()->json($tasks);
         }else{
             return response()->json('The token does not match');
