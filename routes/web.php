7e62e47dbba42516f8b5dd1e6efca97f4531487b
djedai.genius@gmail.com <djedai.genius@gmail.com>
2017-10-10 16:11:46 +0300

    findTaskByDate

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ $router->get('/', function () use ($router) {
 
 $router->group(['prefix' => 'api/v1/'], function() use ($router)
 {
-    $router->get('day/{date}','TaskController@findByDate');
+    $router->get('tasks/{date}','TaskController@findByDate');
 
     $router->post('task','TaskController@createTask');
 
