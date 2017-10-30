52bf5210c5fd122ffb75876e5033972eb3f21ee5
djedai.genius@gmail.com <djedai.genius@gmail.com>
2017-10-30 13:53:42 +0300

    oAuth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,6 +15,18 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
 
+    public function loginAuth()
+    {
+        $s = file_get_contents('http://ulogin.ru/token.php?token=' . $_POST['token'] . '&host=' . $_SERVER['HTTP_HOST']);
+//        $user = json_decode($s, true);
+//        $user['network'] - соц. сеть, через которую авторизовался пользователь
+//        $user['identity'] - уникальная строка определяющая конкретного пользователя соц. сети
+//        $user['first_name'] - имя пользователя
+//        $user['last_name'] - фамилия пользователя
+        return response()->json($s);
+    }
+
+
     public function index(Request $request)
     {
         $token = $request->input('token');
