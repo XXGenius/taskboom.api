8a5027c42cae62216b82a2d8e7c940b6f4c34b53
genius <djedai.genius@gmail.com>
2017-12-12 14:57:57 +0300

    method Create Cycle

--- app/Http/Controllers/CycleController.php
+++ app/Http/Controllers/CycleController.php
@@ -19,7 +19,6 @@ class CycleController extends Controller
         $token = $request->input('token');
         if($token == $this->token){
             $cycle = new Cycle([
-                'user_id' => 1,
                 'date_start' => date('mm-dd-yyyy', 1),
                 'date_end' => date('mm-dd-yyyy', +1),
                 'length_cycle_id' => 2
