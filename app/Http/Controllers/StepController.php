489e607fdad0d0cc1df7ac0aab2b8e18f46fc435
genius <djedai.genius@gmail.com>
2017-12-13 16:20:45 +0300

    get Steps method

--- app/Http/Controllers/StepController.php
+++ app/Http/Controllers/StepController.php
@@ -12,6 +12,7 @@ namespace App\Http\Controllers;
 use App\Step;
 use Illuminate\Http\Request;
 
+
 class StepController extends Controller
 {
     public function index(Request $request)
@@ -19,6 +20,18 @@ class StepController extends Controller
         $token = $request->input('token');
         if($token == $this->token){
             $steps  = Step::all();
+            return response()->json($steps);
+        }else{
+            return response()->json('The token does not match');
+        }
+    }
+
+    public function getMySteps (Request $request)
+    {
+        $token = $request->input('token');
+        if($token == $this->token){
+            $cycle_id = $request->input('cycle_id');
+            $steps  = Step::where('cycle_id','=',$cycle_id);
             return response()->json($steps);
         }else{
             return response()->json('The token does not match');
