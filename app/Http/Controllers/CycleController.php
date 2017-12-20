8ad1443acdf70f92f96e9b524acff9db7ff1e874
genius <djedai.genius@gmail.com>
2017-12-20 13:27:00 +0300

    Rewards

--- app/Http/Controllers/CycleController.php
+++ app/Http/Controllers/CycleController.php
@@ -131,7 +131,7 @@ class CycleController extends Controller
         $interval->i; // кол-во минут
         $sDate1 = '2017-12-20';
         $sDate2 = $date;
-        $t =  (strtotime($sDate1) - strtotime($sDate2))/3600/24;
+//        $t =  (strtotime($sDate1) - strtotime($sDate2))/3600/24;
         return response()->json($date);
     }
 
