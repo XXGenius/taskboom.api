b92ed3e26eff673ea5581247cf2190ad286e0cfc
genius <djedai.genius@gmail.com>
2017-12-28 16:18:17 +0300

    day cycle

--- app/Http/Controllers/DayController.php
+++ app/Http/Controllers/DayController.php
@@ -17,11 +17,25 @@ class DayController extends Controller
     public function create(Request $request)
     {
         $token = $request->input('token');
+        $date = $request->input('date');
         if($token == $this->token){
-            $day = Day::create($request->all());
+            $day = new Day([
+                'user_id' => $request->input('user_id'),
+                'date' => $date,
+            ]);
+            $day->save();
             return response()->json($day);
         }else{
             return response()->json('The token does not match');
         }
+    }
+
+    public function getDay(Request $request)
+    {
+        $user_id = $request->input('user_id');
+        $date = $request->input('date');
+        $day  = Day::where([['date','=',$date],['user_id','=', $user_id]])->get();
+        return $day;
+
     }
 }
